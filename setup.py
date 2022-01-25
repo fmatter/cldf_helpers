c19f70c3c673ab2101d9bb033430909539892678
Florian Matter <florianmatter@gmail.com>
2022-01-25 09:56:19 +0100

    restructured

--- setup.py
+++ setup.py
@@ -1,2 +1,26 @@
-from setuptools import setup
-setup()
+import setuptools
+
+setuptools.setup(
+    name="cldf_helpers",
+    version="0.0.1",
+    author="Florian Matter",
+    author_email="florianmatter@gmail.com",
+    description="An eclectic collection of functions in some related to a CLDF workflow",
+    url="https://github.com/fmatter/cldf_helpers",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+    "cldfbench",
+    "pycldf",
+    "pyperclip",
+    "pyglottolog"
+    ],
+    entry_points = {
+        'console_scripts': ['cldfhtex=cldf_helpers.cldfhtex:main'],
+    },
+    python_requires='>=3.6',
+)
